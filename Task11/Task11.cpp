bb5cd4a2f54575f5d855f1ae3b2aa32ea3f78d02
DenisPadalko <93438147+DenisPadalko@users.noreply.github.com>
2023-04-13 21:43:33 +0300

    add chain of responsibility pattern

--- Task11/Task11.cpp
+++ Task11/Task11.cpp
@@ -1,4 +1,5 @@
 #include "Decorator.h"
+#include "ChainOfResponsibility.h"
 
 int main(int argc, char* argv[])
 {
@@ -8,6 +9,10 @@ int main(int argc, char* argv[])
 
     SomePizza = new ExtraCheese(SomePizza);
     std::cout << SomePizza->GetDescription() << " costs $" << SomePizza->GetPrice() << std::endl;
+    std::cout << std::endl << std::endl;
 
+    //Chain of responsibility design pattern
+    ATM SomeATM(200, 400, 700);
+    SomeATM.GiveMoney(275);
     return 0;
 }
